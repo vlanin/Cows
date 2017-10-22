8e8f86f35d2ee4974d2a1deb57fedc5cea05ffb3
LocalUser <LocalUser@K3-511-17>
2017-10-22 15:08:56 +0500

    Добавьте файлы проекта.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Коровы;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int n = 1;
+            string expected = "1 корова";
+
+            string actual = Program.CowCounter(n);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod11()
+        {
+            int n = 11;
+            string expected = "11 коров";
+
+            string actual = Program.CowCounter(n);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod111()
+        {
+            int n = 111;
+            string expected = "111 коров";
+
+            string actual = Program.CowCounter(n);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Коровы/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Коровы
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine(CowCounter(45));
+        }
+
+        public static string CowCounter(int n)
+        {
+            int lastDigit = n % 10;
+            int lastTwoDigits = n % 100;
+            string result = string.Empty;
+
+            if (11<=lastTwoDigits && lastTwoDigits <= 19)
+                result = "коров";
+            else if (lastDigit == 1)
+                result = "корова";
+            else if (2<=lastDigit && lastDigit<=4)
+                result = "коровы";
+            else if (5<=lastDigit && lastDigit<=9 ||
+                lastDigit ==0)
+                result = "коров";
+
+            return string.Format("{0} {1}", n, result);
+        }
+
+    }
+}
